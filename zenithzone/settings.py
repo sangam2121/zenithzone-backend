b9043a0b012945ccb95f41192aa10c2621d27990
sangamb <SANGAMB035@GMAIL.COM>
2024-01-06 12:41:26 +0545

    chore: cors header issues fixed

--- zenithzone/settings.py
+++ zenithzone/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = ["http://127.0.0.1",
-                        "http://localhost:3000", "http://localhost:8000"]
+                        "http://localhost:3000", "http://localhost:8000", "http://localhost:5173"]
 
 CORS_ALLOW_HEADERS = (
     "accept",
