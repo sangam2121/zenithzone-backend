ee11873c6bb9e49690c4953f2330b6261d00002a
sangamb <SANGAMB035@GMAIL.COM>
2024-01-06 11:17:30 +0545

    chore: cors header

--- zenithzone/settings.py
+++ zenithzone/settings.py
@@ -56,6 +56,29 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1",
+                        "http://localhost:3000", "http://localhost:8000"]
+
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
+
 
 ROOT_URLCONF = 'zenithzone.urls'
 
