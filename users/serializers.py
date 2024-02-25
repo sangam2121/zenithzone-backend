7f18c29b269688f6d35c0d1b40524b610a48dae5
sangamb <SANGAMB035@GMAIL.COM>
2024-02-25 21:37:33 +0545

    payload for jwt added (user_id, name, email, type)

--- users/serializers.py
+++ users/serializers.py
@@ -13,7 +13,14 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         # Add custom claims
         token['email'] = user.email
+        token['user_id'] = str(user.id)
+        token['user_name'] = user.first_name + ' ' + user.last_name
+        token['user_type'] = user.user_type
         return token
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
 
 
 class RegisterSerializer(serializers.ModelSerializer):
