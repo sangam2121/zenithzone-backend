94b40310feba166c4c3d5d1a0eba02c0046df368
sangamb <SANGAMB035@GMAIL.COM>
2024-04-06 12:56:08 +0545

    chatrooms and message fetch api

--- chat/urls.py
+++ chat/urls.py
@@ -1,24 +1,9 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import ChatRoomCreateAPIView, MessageCreateAPIView, MessageListAPIView, ChatRoomListAPIView
-
-
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('chatrooms/create', ChatRoomCreateAPIView.as_view(), name='chatrooms-create'),
-#     path('chatrooms/', ChatRoomListAPIView.as_view(), name='chatrooms-list'),
-#     path('messages/create', MessageCreateAPIView.as_view(), name='messages-create'),
-#     path('messages/', MessageListAPIView.as_view(), name='messages-list'),
-
-# ]
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MessageViewSet
+from .views import MessageListAPIView, ChatRoomListAPIView
 
-router = DefaultRouter()
-router.register(r'(?P<chat_room_id>[0-9a-f-]+)/messages',
-                MessageViewSet, basename='messages')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('chatrooms/', ChatRoomListAPIView.as_view(), name='chatrooms-list'),
+    path('messages/', MessageListAPIView.as_view(), name='messages-list'),
+]
